a008a5d2004344a3ba273e32bf395c2e3df5d13d
biografimiroslav <slav.kuki@gmail.com>
2026-01-20 19:29:22 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <div className="App"></div>
+        <header className="App-header">
+          <h1>Welcome to My React App</h1>
+        </header>
+    </div > 
   );
 }
 
