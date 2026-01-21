3b43c1f7c62660df60564ad1270d2319c788baff
CHUPAKABRA <147237968+ZenyaKrymovskiy@users.noreply.github.com>
2026-01-21 21:03:26 +0200

    add small style footer( mobile version) and bug fixes with addres in footer. Bug fixed with slider on tablet and phone

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Footer.css';
+import facebook from '../images/facebook.png';
+import logo from '../images/logo.png';
+import whatsapp from '../images/whatsapp.png';
+import  instagram from '../images/instagram.png';
 
 const Footer = () => {
   return (
@@ -7,44 +11,47 @@ const Footer = () => {
       <div className="footer__container">
         
         <div className="footer__logo-box">
-          <div className="logo">
-          </div>
+          <img src={logo} alt="Company logo" className="footer__logo" />
         </div>
 
-        <section className="footer__address-section">
-          <h3 className="footer__title">Address</h3>
-          <address className="footer__address-content" href="https://www.google.com/maps/search/+Svobody+str.+35+++++++++++++Kyiv+++++++++++++Ukraine/@50.4413976,30.5218932,7691m/data=!3m1!1e3?entry=ttu&g_ep=EgoyMDI2MDExMy4wIKXMDSoASAFQAw%3D%3D">
-            Svobody str. 35
-            Kyiv
-            Ukraine
-          </address>
-        </section>
+       <section className="footer__address-section">
+  <h3 className="footer__title">Address</h3>
+  <address className="footer__address-content">
+    <a
+      href="https://www.google.com/maps/search/Svobody+str.+35+Kyiv+Ukraine"
+      className="footer__address-link"
+    >
+      Svobody str. 35
+      <br />
+    Kyiv<br />
+    Ukraine
+    </a>
+    
+  </address>
+</section>
 
         <section className="footer-join">
           <h3 className="footer-join__title">Contact us</h3>
           <ul className="social-networks-list">
             
             <li className="social-networks-list__item">
-              <a href="#" className="social-networks-list__link">
-                <svg className="social-networks-list__icon">
-                  <use href="."></use>
-                </svg>
+              <a href="https://www.instagram.com/" className="social-networks-list__link">
+                <img src={instagram} alt="WhatsApp" className="social-networks-list__icon" />
+
               </a>
             </li>
 
             <li className="social-networks-list__item">
-              <a href="#" className="social-networks-list__link ">
-                <svg className="social-networks-list__icon">
-                  <use href="."></use>
-                </svg>
+              <a href="https://www.facebook.com/?locale=uk_UA" className="social-networks-list__link ">
+                <img src={facebook} alt="WhatsApp" className="social-networks-list__icon" />
+
               </a>
             </li>
 
             <li className="social-networks-list__item">
-              <a href="#" className="social-networks-list__link ">
-                <svg className="social-networks-list__icon">
-                  <use href="."></use>
-                </svg>
+              <a href="https://www.whatsapp.com/?lang=uk" className="social-networks-list__link ">
+                <img src={whatsapp} alt="WhatsApp" className="social-networks-list__icon" />
+
               </a>
             </li>
 
--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Slider from './slider/Slider';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Slider/>
+    <Footer />
   </React.StrictMode>
 );
 
