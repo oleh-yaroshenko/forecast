d8b00ac00e3bf4f4d95ce10f81799a7b37954ad3
makar arzumanian <makar2009on@gmail.com>
2026-01-22 17:17:39 +0100

    added first 2 cards

--- src/WeatherDashboard.jsx
+++ src/WeatherDashboard.jsx
@@ -1,36 +1,50 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import "./WeatherDashboard.css";
+import termometer from "./images-weather/termometer.svg";
+import clowd from "./images-weather/clowd.svg";
+import pressure from "./images-weather/pressure.svg";
+import wind from "./images-weather/wind.svg";
+import visibility from "./images-weather/visibility.svg";
 
 const API_KEY = "533d7532d61d36db17cc95c0414c1870";
+const CITY = "Kyiv";
 
-const countries = {
-  ua: { name: "Ukraine", city: "Kyiv" },
-};
-
-function WeatherDashboard() {
-  const [country, setCountry] = useState("ua");
+export default function WeatherDashboard() {
   const [weather, setWeather] = useState(null);
 
   useEffect(() => {
-    const city = countries[country].city;
-
     fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${CITY}&appid=${API_KEY}&units=metric`
     )
-      .then((res) => res.json())
-      .then((data) => setWeather(data));
-  }, [country]);
+      .then(res => res.json())
+      .then(data => setWeather(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  if (!weather) return <p>Loading weather...</p>;
 
   return (
-    <div>
-      <div className="mb-4">
-        <select
-          value={country}
-          onChange={(e) => setCountry(e.target.value)}
-        >
-          {Object.entries(countries).map(([key, value]) => (
-            <option key={key} value={key}>
-              {value.name}
-            </option>
-          ))}
-        </select>
-      </div>
+    <div className="weather-grid">
+      <WeatherCard title="Feels like">
+        {weather.main.feels_like.toFixed(1)}°C
+        <br />
+        <img src={termometer} alt="" />
+      </WeatherCard>
+
+      <WeatherCard title="Min °C">
+        {weather.main.temp_min.toFixed(1)}°C
+        <br />
+       <small>Max °C</small>  {weather.main.temp_max.toFixed(1)}°C
+      </WeatherCard>
+    </div>
+  );
+}
+
+function WeatherCard({ title, children }) {
+  return (
+    <div className="weather-card">
+      <p className="weather-title">{title}</p>
+      <div className="weather-value">{children}</div>
+    </div>
+  );
+}
