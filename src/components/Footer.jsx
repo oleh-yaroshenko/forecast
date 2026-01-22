f693821e6f487ada6ff56b2f4d3b2b6e1e8eda46
CHUPAKABRA <147237968+ZenyaKrymovskiy@users.noreply.github.com>
2026-01-22 20:20:12 +0200

    First adaptive styles in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,9 @@ import facebook from '../images/facebook.png';
 import logo from '../images/logo.png';
 import whatsapp from '../images/whatsapp.png';
 import  instagram from '../images/instagram.png';
-
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+</style>
 const Footer = () => {
   return (
     <footer className="footer">
