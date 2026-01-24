6ee41aeff7867b15e692f9af01ea493a6883265d
Isaiev Mark <isaev24102011@gmail.com>
2026-01-24 08:00:38 +0200

    трішки логіки

--- src/components/ModalForm.jsx
+++ src/components/ModalForm.jsx
@@ -1,29 +1,69 @@
+import { useRef, useState } from 'react'
 import '../modalForm.css'
 
-function ModalForm(){
-    return(
-        <form action="" className="form">
-            <h2 className='form-h2'>Sing up</h2>
+function ModalForm() {
+    const [value, setValue] = useState(true)
+    const Form = useRef()
+    const Form2 = useRef()
 
-            <div className="form-box">
-                <label className='form-label' htmlFor="username">Username</label>
-                <input className='form-input' required placeholder='Username' type="text" name="" id="username" />
-            </div>
+    function handleClose() {
+        Form.current.classList.toggle('close')
+    }
 
-            <div className="form-box">
-                <label className='form-label' htmlFor="E-Mail">E-Mail</label>
-                <input className='form-input' required placeholder='E-Mail' type="text" name="" id="E-Mail" />
-            </div>
+    function handleCloseA(){
+        setValue(false)
+    }
 
-            <div className="form-box">
-                <label className='form-label' htmlFor="Password">Password</label>
-                <input className='form-input' required placeholder='Password' type="password" name="" id="Password" />
-            </div>
+    return (
+        <>
+            <form ref={Form} action="" className={value == true ? "form" : "form close"}>
+                <h2 className='form-h2'>Sing up</h2>
+                <span onClick={handleClose} className='closeModal'>✖</span>
 
-            <button className="form-btn">Sing up</button>
+                <div className="form-box">
+                    <label className='form-label' htmlFor="username">Username</label>
+                    <input className='form-input' required placeholder='Username' type="text" name="" id="username" />
+                </div>
 
-            <p className='form-p'>Already have an account? <a className='form-a' href="#">Log In</a></p>
-        </form>
+                <div className="form-box">
+                    <label className='form-label' htmlFor="E-Mail">E-Mail</label>
+                    <input className='form-input' required placeholder='E-Mail' type="text" name="" id="E-Mail" />
+                </div>
+
+                <div className="form-box">
+                    <label className='form-label' htmlFor="Password">Password</label>
+                    <input className='form-input' required placeholder='Password' type="password" name="" id="Password" />
+                </div>
+
+                <button className="form-btn">Sing up</button>
+
+                <p className='form-p'>Already have an account? <a onClick={handleCloseA} className='form-a' href="#">Log In</a></p>
+            </form>
+
+            <form ref={Form2} action="" className={value == true ? "form2 close" : "form2 open"}>
+                <h2 className='form-h2'>Sing up</h2>
+                <span onClick={handleClose} className='closeModal'>✖</span>
+
+                <div className="form-box">
+                    <label className='form-label' htmlFor="username">Username</label>
+                    <input className='form-input' required placeholder='Username' type="text" name="" id="username" />
+                </div>
+
+                <div className="form-box">
+                    <label className='form-label' htmlFor="E-Mail">E-Mail</label>
+                    <input className='form-input' required placeholder='E-Mail' type="text" name="" id="E-Mail" />
+                </div>
+
+                <div className="form-box">
+                    <label className='form-label' htmlFor="Password">Password</label>
+                    <input className='form-input' required placeholder='Password' type="password" name="" id="Password" />
+                </div>
+
+                <button className="form-btn">Sing up</button>
+
+                <p className='form-p'>Don't have an account? <a onClick={handleClose} className='form-a' href="#">Sing up!</a></p>
+            </form>
+        </>
     )
 }
 
