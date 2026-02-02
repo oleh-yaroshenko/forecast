12578afde4bfa87e171b1fbaca3be0611d5c4f4d
CHUPAKABRA <147237968+ZenyaKrymovskiy@users.noreply.github.com>
2026-02-02 17:21:32 +0200

    new code slider

--- src/slider/Slider.jsx
+++ src/slider/Slider.jsx
@@ -1,47 +1,53 @@
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, EffectCoverflow } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
 import Sliderone from '../images/Sliderone.jpg';
 import Slider2 from '../images/Slider2.jpg';
 import Slider3 from '../images/Slider3.jpg';
 import Slider4 from '../images/Slider4.jpg';
 import Slider5 from '../images/Slider5x.jpg';
-import { useEffect, useState } from 'react';
-import '../components/Slider.css'
+import '../components/Slider.css';
 const images = [
-  { id: 1, src: Sliderone, alt: 'Waterfall' },
-  { id: 2, src: Slider2, alt: ' river' },
-  { id: 3, src: Slider3, alt: 'Valley ' },
-  { id: 4, src: Slider4, alt: ' mountains' },
-  { id: 5, src: Slider5, alt: 'lake' },
+  { id: 1, src: Sliderone },
+  { id: 2, src: Slider2 },
+  { id: 3, src: Slider3 },
+  { id: 4, src: Slider4 },
+  { id: 5, src: Slider5 },
 ];
+
  function Slider() {
-const [currentIndex, setCurrentIndex] = useState(2);
-useEffect(() => {
-  const interval = setInterval(() => {
-    setCurrentIndex((prev) => (prev + 1) % images.length);
-  }, 4000);
+  return (
+    <section className="Nature">
+      <div className="container">
+        <h3 className="Nature__title">Beautiful nature</h3>
 
-  return () => clearInterval(interval);
-}, []);
-
-    return(
-        <section className='Nature'>
-            <div className='container'>
- <h3 className='Nature__title'>Beautiful nature</h3>
-      <div className='Nature-slider'>
-        {images.map((img, i) => (
-          <div
-            key={img.id}
-            className={`slide ${i === currentIndex ? 'slide--active' : ''}`}
-            onClick={() => setCurrentIndex(i)}
-          >
-            <img src={img.src} alt={img.alt} className="slide-media" />
-          </div>
-        ))}
-
+        <Swiper
+          modules={[EffectCoverflow, Autoplay]}
+          effect="coverflow"
+          grabCursor={true}
+          centeredSlides={true}
+        loop={true}
+          slidesPerView="auto" 
+          autoplay={{ delay: 5000, disableOnInteraction: false }}
+          coverflowEffect={{
+            rotate: 0,   
+            stretch: 0,
+            depth: 100,     
+            modifier: 2.5,
+            slideShadows: false, 
+          }}
+          className="Nature-slider"
+        >
+          {images.map((img) => (
+            <SwiperSlide key={img.id} className="slide-item">
+              <img src={img.src} alt="nature" className="slide-image" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-            </div>
-           
-
-        </section>
-    )
- }
- export default Slider
+    </section>
+  );
+}
+export default Slider
